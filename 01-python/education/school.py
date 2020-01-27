77d8230df2de151a6d353057cfb11d3f226e61a6
Taylor Hazlett <thazlett@bu.edu>
2020-01-27 17:13:29 -0500

    added schools to the city

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,5 @@
-def draw_school():
-    print("school not found")
-    return
+def draw_school(level=["preschool","elementary","middle","high school", "college"]):
+	for l in level:
+		print("[" + l + "]")
+	print()
+	return
